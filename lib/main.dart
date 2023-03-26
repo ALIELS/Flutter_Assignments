24b7d69ae78d3866d06e510e4d5ad6c6f17e902b
ALIELS <am5434@fayoum.edu.eg>
2023-03-26 20:51:44 +0200

    First Assignment Commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home:  Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Application'),
+        backgroundColor: Colors.lightGreen),
+      body: const Center(
+          child: Text('Ali Mohammed Ali',)) ,
+
+    );
+  }
+}
